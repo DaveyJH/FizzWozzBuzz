ca1bf013336a7f94fc2430903dc52cd88aded61f
DaveyJH <daveyj.dh@googlemail.com>
2021-10-26 22:08:06 +0100

    docs: test.py initial commit

---
+++ test.py
@@ -0,0 +1,31 @@
+"""
+A doctsring explaining what the file does
+"""
+
+
+def example():
+    """
+    does some stuff
+    """
+
+    age = 16
+
+    while age <= 18:
+        if age <= 18:
+            age += 1
+            print(age)
+        else:
+            raise SystemExit('You must be older than 18!')
+
+example()
+
+print("Hello world! is a", type("Hello, World!"))
+print(42, "is a", type(42))
+print(3.145, "is a", type(3.145))
+print(1j, "is a", type(1j))
+print(["egg", "bacon", "spam"], "is a", type(["egg", "bacon", "spam"]))
+print(("egg", "bacon", "spam"), "is a", type(("egg", "bacon", "spam")))
+print(range(6), "is a", type(range(6)))
+print({"name" : "John", "age" : 80}, "is a", type({"name" : "John", "age" : 80}))
+print({"egg", "bacon", "spam"}, "is a", type({"egg", "bacon", "spam"}))
+print(True, "is a", type(True))
